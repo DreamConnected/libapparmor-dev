41eae8986947a9399edb33b166529b59e4e82641
Christian Boltz <apparmor@cboltz.de>
2018-07-25 20:44:39 +0200

    split off add_or_remove_flag() from change_profile_flags()
    Also add some tests for add_or_remove_flag()
    
    (cherry picked from commit 604004c2b68fee17f804701fab22c0a9470c457d +
     conflict resolution)

--- utils/apparmor/profile_storage.py
+++ utils/apparmor/profile_storage.py
@@ -116,3 +116,15 @@ def split_flags(flags):
     flags_list = flags.replace(',', ' ').split()
     # sort and remove duplicates
     return sorted(set(flags_list))
+
+def add_or_remove_flag(flags, flag_to_change, set_flag):
+    '''add (if set_flag == True) or remove the given flag_to_change to flags'''
+
+    if set_flag:
+        if flag_to_change not in flags:
+            flags.append(flag_to_change)
+    else:
+        if flag_to_change in flags:
+            flags.remove(flag_to_change)
+
+    return sorted(flags)
